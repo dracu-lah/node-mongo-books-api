d05eefef696c150f28f23da79eea12570684628f
dracu-lah <nevilnicks4321@gmail.com>
2022-11-09 06:18:35 +0530

    created an books api using nodejs  and mongo db

---
+++ index.js
@@ -0,0 +1,23 @@
+const MongoClient = require("mongodb").MongoClient;
+const connection_string = "mongodb://localhost:27017/";
+const express = require("express");
+const app = express();
+
+MongoClient.connect(
+  connection_string,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    if (err) throw error;
+    console.log("Connected to mongo db");
+    var db = client.db("blog");
+    app.get("/books", (req, res) => {
+      db.collection("books")
+        .find({})
+        .toArray((err, data) => {
+          if (err) throw err;
+          res.send(data);
+        });
+    });
+    app.listen(8000);
+  }
+);
---
+++ readme.txt
@@ -0,0 +1,4 @@
+npm install mongodb
+npm install express
+
+use localhost:8000/books to access books
